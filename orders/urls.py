97dd998040de0dd62444112a7270445563077aee
Matt Hupy <matthupy@gmail.com>
2020-06-27 12:57:14 -0500

    Working Order Success page Working Place Order button Working Clear Cart button

--- orders/urls.py
+++ orders/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("addToCart", views.addToCart, name="addToCart"),
+    path("clearCart", views.clearCart, name="clearCart"),
+    path("placeOrder", views.placeOrder, name="placeOrder"),
     path("checkout", views.checkout, name="checkout"),
-    path("cart", views.cart, name="cart")
+    path("cart", views.cart, name="cart"),
+    path("orderSuccess", views.orderSuccess, name="orderSuccess")
 ]
--- orders/views.py
+++ orders/views.py
@@ -82,11 +82,27 @@ def addToCart(request):
             productId = product['name']
             productName = Sub.objects.values_list('name').get(pk=productId)[0]
             productSize = product['size']
+            productPrice = Sub.objects.values('price').filter(name=productName).filter(size=productSize).first()
+            productPrice = productPrice['price']
             productExtras = SubExtra.objects.filter(id__in=product['extras'])
             productQuantity = product['quantity']
 
             subObject = Sub.objects.filter(name=productName).filter(size=productSize).first()
             subExtraObjects = SubExtra.objects.filter(id__in=product['extras'])
+
+            # Add the price of the extras
+            extras_cost = SubExtra.objects.filter(id__in=product['extras']).aggregate(Sum('price'))
+            print(extras_cost)
+            print(productPrice)
+            productPrice += extras_cost['price__sum']
+
+            subObject = Sub.objects.create(name=productName, size=productSize, price=productPrice)
+
+            print(subExtraObjects)
+
+            subObject.extras.set(subExtraObjects)
+
+            print(subObject.extras.all())
 
             subExtraCost = 0
             for subExtraObject in subExtraObjects:
@@ -101,15 +117,101 @@ def addToCart(request):
                 userCart = Cart.objects.create(username=userObject)
 
             for x in range(int(productQuantity)):
-                userCart.subs.add(Sub.objects.get(id=subObject.id))
-                userCart.subExtras.add(*subExtraObjects)
+                userCart.subs.add(subObject)
+                #userCart.subExtras.add(*subExtraObjects)
 
         elif productType == 'Pizza':
-            productId = product['name']
-            productName = Pizza.objects.values_list('name').get(pk=productId)[0]
+            productStyle = product['style']
+            pizzaType = product['type']
+            productExtras = product['extras']
             productSize = product['size']
-            pizzaObject = Pizza.objects.filter(name=productName).filter(size=productSize).first()
+            productToppings = product['toppings']
             productQuantity = product['quantity']
+
+            pizzaType = ""
+            if (len(productToppings) == 0):
+                pizzaType = "CH"
+            elif (len(productToppings) == 1):
+                pizzaType = "1"
+            elif (len(productToppings) == 2):
+                pizzaType = "2"
+            elif (len(productToppings) == 3):
+                pizzaType = "3"
+            else:
+                pizzaType = "SP"
+
+            # Set prices
+            pizzaPrice = 0
+            if (pizzaType == "CH"):
+                if productStyle == "R" and productSize == "S":
+                    pizzaPrice = 12.70
+                elif productStyle == "R" and productSize == "L":
+                    pizzaPrice = 17.95
+                elif productStyle == "S" and productSize == "S":
+                    pizzaPrice = 24.45
+                elif productStyle == "S" and productSize == "L":
+                    pizzaPrice = 38.70
+                else:
+                    raise Exception("Pizza price not found!")
+            elif (pizzaType == "1"):
+                if productStyle == "R" and productSize == "S":
+                    pizzaPrice = 13.70
+                elif productStyle == "R" and productSize == "L":
+                    pizzaPrice = 19.95
+                elif productStyle == "S" and productSize == "S":
+                    pizzaPrice = 26.45
+                elif productStyle == "S" and productSize == "L":
+                    pizzaPrice = 40.70
+                else:
+                    raise Exception("Pizza price not found!")
+            elif (pizzaType == "2"):
+                if productStyle == "R" and productSize == "S":
+                    pizzaPrice = 15.20
+                elif productStyle == "R" and productSize == "L":
+                    pizzaPrice = 21.95
+                elif productStyle == "S" and productSize == "S":
+                    pizzaPrice = 28.45
+                elif productStyle == "S" and productSize == "L":
+                    pizzaPrice = 42.70
+                else:
+                    raise Exception("Pizza price not found!")
+            elif (pizzaType == "3"):
+                if productStyle == "R" and productSize == "S":
+                    pizzaPrice = 16.20
+                elif productStyle == "R" and productSize == "L":
+                    pizzaPrice = 23.95
+                elif productStyle == "S" and productSize == "S":
+                    pizzaPrice = 29.45
+                elif productStyle == "S" and productSize == "L":
+                    pizzaPrice = 44.70
+                else:
+                    raise Exception("Pizza price not found!")
+            elif (pizzaType == "SP"):
+                if productStyle == "R" and productSize == "S":
+                    pizzaPrice = 17.75
+                elif productStyle == "R" and productSize == "L":
+                    pizzaPrice = 25.95
+                elif productStyle == "S" and productSize == "S":
+                    pizzaPrice = 30.45
+                elif productStyle == "S" and productSize == "L":
+                    pizzaPrice = 45.70
+                else:
+                    raise Exception("Pizza price not found!")
+
+            # Add the price of the toppings
+            pizzaPrice += Toppings.objects.filter(id__in=prodcutToppings).aggregate(sum)
+
+            pizza = Pizza.objects.create(style=productStyle, size=productSize, price=pizzaPrice)
+            pizza.toppings.set(productToppings)
+
+            # Get the user's cart
+            userCart = Cart.objects.filter(username=User.objects.get(username=request.user.username)).first()
+
+            if userCart is None:
+                userCart = Cart.objects.create(username=userObject)
+
+            for x in range(int(productQuantity)):
+                userCart.pizzas.add(pizza)
 
 
         elif productType == 'Dinner Platter':
@@ -162,6 +264,7 @@ def addToCart(request):
         #Only accept POST requests here, otherwise redirect back to the home page
         return redirect('index')
 
+    messages.success(request, 'Item added to cart!')
     return redirect('index')
 
 @login_required(login_url='/login/')
@@ -184,7 +287,8 @@ def checkout(request):
                 'dinnerPlatters': cart.dinnerPlatters.all(),
                 'salads': cart.salads.all(),
                 'subs': cart.subs.all(),
-                'pastas': cart.pastas.all()
+                'pastas': cart.pastas.all(),
+                'pizzas': cart.pizzas.all()
             }
 
             return render(request, "orders/checkout.html", context)
@@ -192,6 +296,45 @@ def checkout(request):
             return redirect('index')
     else:
         return render(request, "orders/checkout.html")
+
+@login_required(login_url='/login/')
+def placeOrder(request):
+    if request.method == 'POST':
+        try:
+            # Remove all items from the cart
+            userObject = User.objects.get(username=request.user.username)
+            userCart = Cart.objects.filter(username=userObject).first()
+
+            if userCart is not None:
+                userCart.delete()
+
+            # At the end, redirect to the Order Confirmation page
+            return redirect('index')
+        except:
+            messages.error(request, 'Oops! Something went wrong, please try again.')
+            return redirect('index')
+    else:
+        return redirect('index')
+
+@login_required(login_url='/login/')
+def clearCart(request):
+    if request.method == 'POST':
+        try:
+            # Remove all items from the cart
+            userObject = User.objects.get(username=request.user.username)
+            userCart = Cart.objects.filter(username=userObject).first()
+
+            if userCart is not None:
+                userCart.delete()
+
+            # At the end, redirect to the Order Confirmation page
+            #messages.success(request, 'Success, cart cleared!')
+            return redirect('index')
+        except:
+            messages.error(request, 'Oops! Something went wrong, please try again.')
+            return redirect('index')
+    else:
+        redirect('index')
 
 @login_required(login_url='/login/')
 def cart(request):
@@ -218,3 +361,7 @@ def cart(request):
         return HttpResponse(total)
     else:
         return redirect('index')
+
+@login_required(login_url='/login/')
+def orderSuccess(request):
+    return render(request, "orders/order_success.html")
